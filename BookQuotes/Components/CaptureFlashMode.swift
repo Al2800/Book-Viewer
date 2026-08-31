974a1c3ffce8c2809840da4c1855c5a01e8aa1fb
Al2800 <33057005+Al2800@users.noreply.github.com>
2026-08-31 09:28:06 +0100

    feat(camera): map capture flash modes to AVFoundation

--- BookQuotes/Components/CaptureFlashMode.swift
+++ BookQuotes/Components/CaptureFlashMode.swift
@@ -1,3 +1,4 @@
+import AVFoundation
 import Foundation
 
 enum CaptureFlashMode: CaseIterable {
@@ -13,11 +14,27 @@ enum CaptureFlashMode: CaseIterable {
         }
     }
 
+    var accessibilityLabel: String {
+        switch self {
+        case .auto: return "Flash automatic"
+        case .on: return "Flash on"
+        case .off: return "Flash off"
+        }
+    }
+
     var next: CaptureFlashMode {
         switch self {
         case .auto: return .on
         case .on: return .off
         case .off: return .auto
+        }
+    }
+
+    var avFoundationMode: AVCaptureDevice.FlashMode {
+        switch self {
+        case .auto: return .auto
+        case .on: return .on
+        case .off: return .off
         }
     }
 }
